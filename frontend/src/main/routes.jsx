c5b1cae569c065eaeb492c58319013a9f2f42d7d
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-30 16:39:30 -0300

    add: routes

---
+++ frontend/src/main/routes.jsx
@@ -0,0 +1,13 @@
+import React from 'react' 
+import { Router, Route, Redirect, hashHistory } from 'react-router' 
+
+import Dashboard2 from '../dashboard2/dashboard2' 
+import BillingCycle from '../billingCycle/billingCycle' 
+
+export default props => ( 
+    <Router history={hashHistory}> 
+        <Route path='/' component={Dashboard2} /> 
+        <Route path='/billingCycles' component={BillingCycle} /> 
+        <Redirect from='*' to='/' />
+    </Router>
+)
