4a2b28f8116fb5edca0158f929b51dfa32f887c8
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-30 16:30:53 -0300

    add: dependencies and header

---
+++ frontend/src/common/template/dependencies.js
@@ -0,0 +1,13 @@
+import 'admin-lte/plugins/jqueryui/jquery-ui.min.js'
+import 'admin-lte/plugins/fastclick/fastclick.js'
+import 'admin-lte/plugins/slimscroll/jquery.slimscroll.min.js'
+import 'admin-lte/dist/js/app.min.js'
+
+import 'font-awesome/css/font-awesome.min.css'
+import 'ionicons/dist/css/ionicons.min.css'
+import 'admin-lte/bootstrap/css/bootstrap.min.css'
+import 'admin-lte/dist/css/AdminLTE.min.css'
+import 'admin-lte/dist/css/skins/_all-skins.min.css'
+import 'admin-lte/plugins/iCheck/flat/blue.css'
+
+import './custom.css'
