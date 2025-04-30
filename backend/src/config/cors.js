8d767e1f44a65d1e8467ba97f0bfbcc1ac44ea58
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-30 16:17:59 -0300

    fix: backend

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,6 +1,6 @@
-module.exports = (req, res, next) => {
-    res.header('Acess-Control-Allow-Origin', '*')
-    res.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.header('Acess-Control-Allow-Methods', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-  }
+module.exports = (req, res, next) => { 
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+}
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
