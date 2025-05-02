d9276562efe5f4a5f5ae9eb269ba38b65cf181fd
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-02 09:04:57 -0300

    add: billingCycleList

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux'
 
 import dashboardReducer from '../dashboard/dashboardReducer'
 import tabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+
+
 
 const rootReducer = combineReducers({ 
     dashboard: dashboardReducer,
-    tab: tabReducer
+    tab: tabReducer,
+    billingCycle: BillingCycleReducer
 }) 
 
 export default rootReducer
