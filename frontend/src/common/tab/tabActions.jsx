d5547d94e0ee9d0bd21eabdff01c5eb7b211ad70
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-02 08:37:48 -0300

    add: tab visibility

--- frontend/src/common/tab/tabActions.jsx
+++ frontend/src/common/tab/tabActions.jsx
@@ -5,3 +5,13 @@ export function selectTab(tabId) {
         payload: tabId
     }
 }
+
+
+export function ShowTabs(...tabIds){
+    const tabsToShow = {}
+    tabIds.forEach(e => tabsToShow[e] = true)
+    return {
+        type: 'TAB_SHOWED',
+        payload: tabsToShow
+    }
+}
