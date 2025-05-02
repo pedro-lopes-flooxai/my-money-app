dc96256f505adcfd74a3b0ec15b17befd774b420
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-02 12:15:00 -0300

    add: billingCycle message

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+import { toastr } from "react-redux-toastr";
+import { reset as resetForm } from 'redux-form'
+import { ShowTabs, selectTab } from "../common/tab/tabActions";
+
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList(){
@@ -10,8 +14,20 @@ export function getList(){
 }
 
 export function create(values){
-    axios.post(`${BASE_URL}/billingCycles`, values)
-    return {
-        type: 'TEMP'
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+            toastr.success('Sucesso', 'Operação realizada com sucesso.')
+            dispatch ([
+                resetForm('billingCycleForm'),
+                getlist(),
+                selectTab('tabList'),
+                ShowTabs('tabList', 'tabCreate')
+            ])
+        })
+        .catch(e => {
+            e.response.data.errors.forEach(error => toastr.error('Erro', error))
+        })
     }
+    
 }
